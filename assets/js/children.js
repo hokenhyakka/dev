73b4521f363b3bf20812958ef10448e82da7b465
kiguchisaki-i <s-kiguchi@comprime.co.jp>
2024-09-10 11:19:49 +0900

    add reference-page

--- assets/js/children.js
+++ assets/js/children.js
@@ -1,7 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const h1Content = document.querySelector('h1').textContent;
-    const currentDirectory = document.querySelector('.currentDirectory');
+document.addEventListener("DOMContentLoaded", function () {
+    const h1Content = document.querySelector("h1").textContent;
+    const currentDirectory = document.querySelector(".currentDirectory");
     if (currentDirectory) {
         currentDirectory.textContent = h1Content;
     }
 });
+
+const tagTextMap = {
+    station: "駅チカ",
+    parking: "駐車場",
+    openAtNight: "19時以降OK",
+    openOnWeekends: "土日祝営業",
+    kids: "キッズスペース",
+    femaleStaff: "女性スタッフ",
+};
+
+const elements = document.querySelectorAll("[data-tag]");
+if (elements.length > 0) {
+    elements.forEach(function (element) {
+        const tagValue = element.getAttribute("data-tag");
+
+        if (tagTextMap[tagValue]) {
+            element.textContent = tagTextMap[tagValue];
+        }
+    });
+}
